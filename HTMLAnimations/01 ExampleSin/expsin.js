e4772d1cf436f623aa6ce66706d285b615e2a09e
Matt Gebert <mattheuu.gebert@gmail.com>
2017-07-31 01:49:38 +1000

    Started HTMLAnimations to attempt to use d3 for live animation datasets, such as a moving timeseries.

---
+++ HTMLAnimations/01 ExampleSin/expsin.js
@@ -0,0 +1,13 @@
+var svg = d3.select("svg.chart");
+
+var svg = d3.select("svg"),
+    margin = {top: 0, right: 0, bottom: 0, left: 0},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var x = d3.scaleTime()
+    .rangeRound([0, width]);
+
+var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
