3dc8b1df086820569dadadaa7813d5124fbd4a3a
Matt Gebert <mattheuu.gebert@gmail.com>
2017-08-12 02:21:29 +1000

    Working out how to integrate menubar js and css files.

---
+++ photaHome/static/photaHome/js/menubar.js
@@ -0,0 +1,6 @@
+
+//----------------Menu bar for home page ------------------
+$('#menubar-' + menubar_items[crnt_pg]).unbind('mouseenter').unbind('mouseleave')
+  .css('background-color','rgba(255,255,255,0.8)')
+  .children().children().css('color','#114455');
+$('.underline').css('background-color','rgba(255,255,255,0.8)');
