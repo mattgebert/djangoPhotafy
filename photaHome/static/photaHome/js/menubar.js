f1f6eeb7737b89168ba578015d3d7cc340cda5ff
Matt Gebert <mattheuu.gebert@gmail.com>
2017-08-12 21:07:37 +1000

    Integrated functioning sekizai. Now works on home page and physics page.

--- photaHome/static/photaHome/js/menubar.js
+++ photaHome/static/photaHome/js/menubar.js
@@ -2,5 +2,8 @@
 //----------------Menu bar for home page ------------------
 $('#menubar-' + menubar_items[crnt_pg]).unbind('mouseenter').unbind('mouseleave')
   .css('background-color','rgba(255,255,255,0.8)')
-  .children().children().css('color','#114455');
-$('.underline').css('background-color','rgba(255,255,255,0.8)');
+  .children().children().children().css('color','#114455').delay(400).queue(function() {
+    $('.underline').css('background-color','rgba(255,255,255,0.8)');
+  });
+
+matchHeightCustom(".subpagelink");
