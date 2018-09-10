59bb9bb352efa5945a340b7ad7d053b45db6d596
Matt Gebert <mattheuu.gebert@gmail.com>
2018-09-10 22:24:25 +1000

    Made bubsrt automatically restart, also remove unnecessary y-axis scroll

--- photaHome/static/photaHome/js/svgBgs/bubsrt.js
+++ photaHome/static/photaHome/js/svgBgs/bubsrt.js
@@ -35,7 +35,6 @@ for (i=0; i<bars; i++){
   };
   data.push(datum);
 }
-var datacopy = data.slice(0,-1);
 
 x.domain(data.map(function(d) { return d.ind; }));
 y.domain([0, d3.max(data, function(d) { return d.val; })*1.5]);
@@ -51,6 +50,7 @@ svg.selectAll(".bar")
       .attr("y", function(d) { return y(d.val); })
       .attr("height", function(d) { return height - y(d.val); });
 
+
 //Draw Dataset
 //Define function to get next datapoint.
 var progress = function() {
@@ -60,16 +60,18 @@ var progress = function() {
   for (i=0; i<bars-1; i++) {
       indexes.push(i);
   }
-
   for (i=1; i< bars; i++) {
     //Select a random index from the list of indexes:
-    j = Math.floor(Math.random()*(indexes.length-1)); //Length-1 due to comparison needed
+    j = Math.floor(Math.random()*(indexes.length)); //Length-1 due to comparison needed
     j = indexes.splice(j,1)[0];
 
     minval = data[j].val;
     val = data[j+1].val;
     if (minval >= val) {
-      // Do nothing, order is correct {High to low}
+      //Data order is correct, but if iterated through all values, then reset:
+      if (i == bars-1) {
+        resetBars();
+      }
     } else {
       var iind = data[j].ind; //Copy existing index
       //Swap elements:
@@ -78,20 +80,64 @@ var progress = function() {
       //Shift positions in list for D3.
       data.splice(j,0,data.splice(j+1,1)[0]);
       i = bars + 1; //End loop
-      // alert(j+""+);
+
+      //Rebind data:
+      var transition = svg.transition().duration(250),
+      delay = 50;
+
+      transition.selectAll(".bar")
+      .delay(delay)
+      .attr("x", function(d) { return x(d.ind) });
     }
   }
 
-  //Rebind data:
-  var transition = svg.transition().duration(250),
-  delay = 50;
+};
+
+var resetBars = function () {
+  //Time to reset!
+  clearInterval(intervalID);
+  //Reset current lists of data.
+  indexes = []
+  var i;
+  for (i=0; i<bars; i++) {
+    indexes.push(i);
+  }
+  for (i=0; i<bars; i++){
+    var j = indexes.splice(Math.floor(Math.random() * indexes.length),1)[0];
+    var amp = Math.exp(-(j**2)/(2*10**2));
+    data[i].ind = j;
+    data[i].val = amp;
+  }
+
+
+  //Apply changes to each data element left to right.
+  var transition = svg.transition().duration(1000);
+  x.domain(data.map(function(d) { return d.ind; }));
 
   transition.selectAll(".bar")
-  .delay(delay)
-  .attr("x", function(d) { return x(d.ind) });
+  .delay(5000) //arbitrary time delay for end of sequence.
+  .attr("x", function(d) { return x(d.ind) })
+  .attr("y", function(d) { return y(d.val) })
+  .attr("height", function(d) { return height - y(d.val); })
+  .call(endAll, start); //Bring back timings
+};
 
+function endAll(transition, callback) {
+  // https://stackoverflow.com/questions/10692100/invoke-a-callback-at-the-end-of-a-transition
+  if (typeof callback !== "function") throw new Error("Wrong callback in endall");
+  if (transition.size() === 0) { callback() }
+  var n=0;
+  transition.each(function() { ++n; })
+  .on('end', function() {
+    if (!--n) {
+      callback.apply(this,arguments);
+    }
+  });
 };
 
 //Set periodic function to progressively generate data.
-var intervalID = setInterval(progress, 300);
-// progress();
+var intervalID;
+function start() {
+  intervalID = setInterval(progress, 300);;
+}
+start();
